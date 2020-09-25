1a8224a20ac8b9dc8636171e58aa11550db11f9f
Antonis <b3liever@yandex.com>
2020-09-25 23:50:36 +0300

    remove early return as redundant

--- systems/handle_events.nim
+++ systems/handle_events.nim
@@ -6,7 +6,6 @@ proc handleEvents*(game: var Game) =
       if event.kind == QuitEvent or (event.kind == KeyDown and
             event.key.keysym.scancode == SDL_SCANCODE_ESCAPE):
          game.isRunning = false
-         return
       elif event.kind == KeyDown and not event.key.repeat:
          case event.key.keysym.scancode
          of SDL_SCANCODE_LEFT, SDL_SCANCODE_A:
