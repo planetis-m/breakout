0a521af0389f8808b7058d1c46307cd7fc256c10
Antonis <b3liever@yandex.com>
2020-10-03 09:52:55 +0300

    add notice

--- smartptrs.nim
+++ smartptrs.nim
@@ -1,21 +1,22 @@
+## Delete once https://github.com/nim-lang/fusion/pull/8 is merged
 type
-  UniquePtr*[T] = object
-    val: ptr T
+   UniquePtr*[T] = object
+      val: ptr T
 
 proc `=destroy`*[T](p: var UniquePtr[T]) =
-  mixin `=destroy`
-  if p.val != nil:
-    `=destroy`(p.val[])
-    deallocShared(p.val)
-    p.val = nil
+   mixin `=destroy`
+   if p.val != nil:
+      `=destroy`(p.val[])
+      deallocShared(p.val)
+      p.val = nil
 
 proc `=`*[T](dest: var UniquePtr[T], src: UniquePtr[T]) {.error.}
 
 proc newUniquePtr*[T](val: sink T): UniquePtr[T] {.nodestroy.} =
-  result.val = cast[ptr T](allocShared(sizeof(T)))
-  result.val[] = val
+   result.val = cast[ptr T](allocShared(sizeof(T)))
+   result.val[] = val
 
 proc `[]`*[T](p: UniquePtr[T]): var T {.inline.} =
-  when compileOption("boundChecks"):
-    assert(p.val != nil, "deferencing nil unique pointer")
-  p.val[]
+   when compileOption("boundChecks"):
+      assert(p.val != nil, "deferencing nil unique pointer")
+   p.val[]
