7de6420db913e17c51becd896f7e3b8cbfb94096
Antonis <b3liever@yandex.com>
2020-10-22 23:24:12 +0300

    Handle alloc without threads:on correctly

--- smartptrs.nim
+++ smartptrs.nim
@@ -7,12 +7,18 @@ proc `=destroy`*[T](p: var UniquePtr[T]) =
    mixin `=destroy`
    if p.val != nil:
       `=destroy`(p.val[])
-      deallocShared(p.val)
+      when compileOption("threads"):
+         deallocShared(p.val)
+      else:
+         dealloc(p.val)
 
 proc `=`*[T](dest: var UniquePtr[T], src: UniquePtr[T]) {.error.}
 
 proc newUniquePtr*[T](val: sink T): UniquePtr[T] {.nodestroy.} =
-   result.val = cast[ptr T](allocShared(sizeof(T)))
+   when compileOption("threads"):
+      result.val = cast[ptr T](allocShared(sizeof(T)))
+   else:
+      result.val = cast[ptr T](alloc(sizeof(T)))
    result.val[] = val
 
 proc `[]`*[T](p: UniquePtr[T]): var T {.inline.} =
