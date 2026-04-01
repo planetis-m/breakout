3f9d7bc8c3b7e5b2753185b2b72ddc139cc679eb
Antonis Geralis <b3lievem@yandex.com>
2026-04-01 16:40:33 +0300

    missing file

---
+++ breakout/pools.nim
@@ -0,0 +1,23 @@
+type
+  Pool*[T, I] = object
+    items: seq[T]
+    freeList: seq[int32]
+
+proc alloc*[T, I](pool: var Pool[T, I]; value: sink T): I {.inline.} =
+  if pool.freeList.len > 0:
+    result = I(pool.freeList.pop())
+    pool.items[result.int] = value
+  else:
+    result = I(pool.items.len)
+    pool.items.add(value)
+
+proc free*[T, I](pool: var Pool[T, I]; idx: I) {.inline.} =
+  pool.items[idx.int] = default(T)
+  doAssert idx.int <= high(int32).int
+  pool.freeList.add(idx.int32)
+
+proc `[]`*[T, I](pool: Pool[T, I]; idx: I): lent T {.inline.} =
+  pool.items[idx.int]
+
+proc `[]`*[T, I](pool: var Pool[T, I]; idx: I): var T {.inline.} =
+  pool.items[idx.int]
